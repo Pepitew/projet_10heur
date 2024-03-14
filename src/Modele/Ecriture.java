69aaa00f6692ee7add880243acf896012450b94e
Clement <clementbessieres.pro@gmail.com>
2024-03-14 10:15:32 +0100

    creation de la fonction de sauvegarde dans un fichier texte des informations

---
+++ src/Modele/Ecriture.java
@@ -0,0 +1,24 @@
+package Modele;
+
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+
+public class Ecriture {
+	String info;
+	public Ecriture(Musique music) {
+		
+	}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
