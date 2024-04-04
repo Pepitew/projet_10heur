fade7abde85cf3b4f4edad1d24021020eb79e430
Clement <clementbessieres.pro@gmail.com>
2024-04-04 09:31:00 +0200

    ecriture ok

--- src/Modele/Ecriture.java
+++ src/Modele/Ecriture.java
@@ -3,6 +3,11 @@ package Modele;
 // exemple
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.NumberFormat.Style;
+import java.util.ArrayList;
+
+import Modele.Musique.STYLE;
+
 import java.io.BufferedWriter;
 import java.io.BufferedReader;
 import java.io.File;
@@ -10,14 +15,14 @@ import java.io.FileReader;
 
 public class Ecriture {
 
-
+	private static String path = "data/info_music/test.txt";
 
 	public static void main(String[] args) {
-		Musique m1 = new Musique("Salut", "je suis l'auteur", 60);
-		Musique m2 = new Musique("sd;jkfls<d", "je suis l'auteur", 60);
-		Musique m3 = new Musique("hjk:;,hj", "je suis l'auteur", 60);
-		Musique m4 = new Musique("djkf", "je suis l'auteur", 60);
-		Musique m5 = new Musique("gdflknnnbm", "je suis l'auteur", 60);
+		Musique m1 = new Musique("Salut", "je suis l'auteur", 60, false, STYLE.ELECTRO);
+		Musique m2 = new Musique("sdjkfls<d", "je suis l'auteur", 60, false, STYLE.ELECTRO);
+		Musique m3 = new Musique("hjkhj", "je suis l'auteur", 60, false, STYLE.ELECTRO);
+		Musique m4 = new Musique("djkf", "je suis l'auteur", 60, false, STYLE.ELECTRO);
+		Musique m5 = new Musique("gdflknnnbm", "je suis l'auteur", 60, false, STYLE.ELECTRO);
 
 
 
@@ -31,45 +36,46 @@ public class Ecriture {
 
 		try {
 			// Creates a FileWriter
-			File file = new File("data/info_music/test");
+			File file = new File(path);
+			System.out.println(path);
 
-			if(file.exists()) {
-
-				FileReader reader = new FileReader(file);
-
-				System.out.println("le fichier existe déjà :");
-
-				BufferedReader fichier = new BufferedReader(reader);
-				String line = fichier.readLine();
-				while (line != null) {
-					System.out.println(line);
-					//parsing manuel -> peupler le modèle
-					line = fichier.readLine();
-					// todo
-				}
-			}
-
-
-		} else {
-
-			System.out.println("Existe pas : data/info_music/inf_music ");				
+			//Création buffered et file writer / reader
 			FileWriter writer = new FileWriter(file);
-			// Creates a BufferedWriter
 			BufferedWriter output = new BufferedWriter(writer);
 
-			// Writes the string to the file
-			output.write(Musique.encoder(music));
+			FileReader reader = new FileReader(file);
+			BufferedReader fichier = new BufferedReader(reader);
 
-			// Closes the writer
+			// récupération de toutes les données existantes
+			ArrayList<String> data = new ArrayList<>();
+
+			String line = fichier.readLine(); // vaut null si le fichier est vide de base
+
+			while(line != null) {
+				System.out.println("lecture de la base de donnée ...");
+				data.add(line);
+			}
+			
+			System.out.println("fin de lecture");
+			data.add(Musique.encoder(music));
+			for(String ligne : data) {
+				System.out.println("ecriture ..");
+				System.out.println(ligne);
+				output.write(ligne);
+				output.newLine();
+			}
+			System.out.println("fin d'enregistrement");
+			
 			output.close();
+			writer.close();
+			reader.close();
 
 		}
-	}
 
-	catch (Exception e) {
-		e.getStackTrace();
-	}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 
-}
+	}
 
 }
