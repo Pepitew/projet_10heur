f5d50c97e9b6422a4a8ae23fd38a320a0cfca44c
Matheo Vigneres <matheo.vigneres@gmail.com>
2024-05-10 01:39:24 +0200

    ajout recommandation

--- src/Modele/Hierarchie.java
+++ src/Modele/Hierarchie.java
@@ -1,6 +1,10 @@
 package Modele;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.TreeSet;
+
+import Modele.Musique.STYLE;
 
 public class Hierarchie {
 	
@@ -38,6 +42,61 @@ public class Hierarchie {
 		}
 		
 		return recherche;
+	}
+	
+	public static TreeSet<Musique> recommandation() {
+		TreeSet<Musique> reco = new TreeSet<Musique>();
+		Map<String, Integer> auteurFav = new HashMap<String, Integer>(); //int : nbOccurence
+		Map<STYLE, Integer> styleFav = new HashMap<Musique.STYLE, Integer>(); //int : nbOccurence
+		
+		/*for (Musique m : Hierarchie.hierarchie) {
+			if (m.isLiked) {
+				if (auteurFav.get(m.getAuteur()) != null) {
+					int valeur = auteurFav.get(m.getAuteur());
+					auteurFav.remove(m.getAuteur());
+					auteurFav.put(m.getAuteur(), valeur++);
+				}
+				else {
+					auteurFav.put(m.getAuteur(), 1);
+				}
+				
+				if (styleFav.get(m.getStyle()) != null) {
+					int valeur = styleFav.get(m.getStyle());
+					styleFav.remove(m.getStyle());
+					styleFav.put(m.getStyle(), valeur++);
+				}
+				else {
+					styleFav.put(m.getStyle(), 1);
+				}
+			}
+		}*/
+		
+		for (Musique m : Hierarchie.hierarchie) {
+		    if (m.isLiked) {
+		        auteurFav.merge(m.getAuteur(), 1, Integer::sum);
+		        styleFav.merge(m.getStyle(), 1, Integer::sum);
+		    }
+		}
+		
+		int drapeau = 0;
+		String auteurMax = "";
+		
+		for (String auteur : auteurFav.keySet()) {
+			if (auteurFav.get(auteur) > drapeau) {
+				auteurMax = auteur;
+				drapeau = auteurFav.get(auteur);
+			}
+		}
+		TreeSet<Musique> recherche = Hierarchie.rechercher(auteurMax);
+		for (int i = 0; i < 5; i++) {
+			if (recherche.size() != 0) {
+				reco.add(recherche.first());
+				reco.remove(reco.first());
+			}
+				
+		}
+		
+		return reco;
 	}
 	
 	public String toString() {
